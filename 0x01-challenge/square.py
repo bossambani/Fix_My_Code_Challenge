656b600afa3b294201823b3fa9a3ccfa4cd1a55c
Boss Ambani <bossambaka@gmail.com>
2024-07-23 04:32:55 -0700

    updated class square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,9 @@
-
 #!/usr/bin/python3
 
 class Square():
-    
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
@@ -20,6 +17,7 @@ class Square():
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
